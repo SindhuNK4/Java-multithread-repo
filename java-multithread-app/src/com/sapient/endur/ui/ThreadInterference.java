cf8d7465780a3ff9dd7478722b0f7efb083c3505
sinnk <sinnk@WKWZTxMq1WsXrkb.global.publicisgroupe.net>
2022-04-25 10:05:18 +0530

    firstcommit

---
+++ java-multithread-app/src/com/sapient/endur/ui/ThreadInterference.java
@@ -0,0 +1,57 @@
+package com.sapient.endur.ui;
+
+public class ThreadInterference {
+
+	public static void main(String[] args) {
+		//Without Synchronization
+
+		//		StringBuilder sb= new StringBuilder("welcome");
+		//		
+		//		Thread t1= new Thread( ()->{
+		//			int length= sb.length();
+		//			for(int i=0;i<length;i++) {
+		//				System.out.println( Thread.currentThread().getName() +" "+ sb.append('x'));
+		//			}
+		//		});
+		//		
+		//		
+		//		Thread t2= new Thread( ()->{
+		//			int length= sb.length();
+		//			for(int i=0;i<length;i++) {
+		//				System.out.println( Thread.currentThread().getName() +" "+ sb.append('x'));
+		//			}
+		//		});
+		//		
+		//		t1.start();
+		//		t2.start();
+
+
+		//With synchronization	
+
+		StringBuilder sb= new StringBuilder("welcome");
+
+		Thread t1= new Thread( ()->{
+			int length= sb.length();
+			synchronized(sb) {
+				for(int i=0;i<length;i++) {
+					System.out.println( Thread.currentThread().getName() +" "+ sb.append('x'));
+				}
+			}
+		});
+
+
+		Thread t2= new Thread( ()->{
+			int length= sb.length();
+			synchronized(sb) {
+				for(int i=0;i<length;i++) {
+					System.out.println( Thread.currentThread().getName() +" "+ sb.append('x'));
+				}
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+	}
+
+}
